ef2520c56de5e933c35696b36c819640a22b8671
Pablo Díaz <pablo.pitchu@gmail.com>
2026-03-31 13:25:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4001326996-55305",
+  "appId": "1:24355336681:web:bc0f63605cb4478a57db78",
+  "apiKey": "AIzaSyCX9BOQAdtKJsyVpEAkmRaTnNm0SHLqDLM",
+  "authDomain": "studio-4001326996-55305.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "24355336681"
+};
