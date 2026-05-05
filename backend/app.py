150e3b60be7ceafb16e12af5dd181d6f2868ded7
PabloProst <pabloprost16@gmail.com>
2026-05-05 15:09:04 +0200

    fix: api problem fixed

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-# Permitimos que Next.js (que suele usar el puerto 3000) se conecte
 CORS(app)
+
+@app.route('/')
+def home():
+    return "API de Virus Alert funcionando 🚀"
 
 @app.route('/api/alerta', methods=['GET'])
 def enviar_alerta():
-    # Este es un ejemplo de datos que tu frontend recibirá
     datos = {
         "estado": "peligro",
         "mensaje": "¡Alerta de Virus detectada en el sistema!",
@@ -16,5 +18,4 @@ def enviar_alerta():
     return jsonify(datos)
 
 if __name__ == '__main__':
-    # Ejecutamos en el puerto 5000
     app.run(debug=True, port=5000)
